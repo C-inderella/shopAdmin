d6e7c6ecb509e9ee7ef5341cd2f25ee9369e1d78
C-inderella <929146306@qq.com>
2018-05-17 21:22:24 +0800

    侧边栏样式设置

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,17 +1,92 @@
 <template>
-  <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
-      </el-container>
+  <el-container class="container">
+    <el-header class="header">Header</el-header>
+    <el-container class="container">
+      <el-aside class="aside" width="200px">
+        <el-menu
+          default-active="2"
+          class="nav-menu"
+          :unique-opened="true"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">用户列表</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">角色列表</el-menu-item>
+            <el-menu-item index="2-2">权限列表</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="3-1">商品列表</el-menu-item>
+            <el-menu-item index="3-2">分类参数</el-menu-item>
+            <el-menu-item index="3-3">商品分类</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="4-1">订单列表</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="5-1">数据报表</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
     </el-container>
+  </el-container>
 </template>
 <script>
 export default {
   data () {
-    return {}
+    return {
+
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
-<style></style>
+<style>
+  .header {
+    background-color: #b3c1cd;
+  }
+
+  .container, .aside, .main, .aside .nav-menu {
+    height: 100%;
+  }
+
+  .aside {
+    background-color: #d4dfe4;
+  }
+
+  .main {
+    background-color: #eaeef1;
+  }
+</style>
