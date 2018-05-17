09a55fb443e712f9f1b2f981d0f18edd7679136a
C-inderella <929146306@qq.com>
2018-05-17 22:21:18 +0800

    完成主页子路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/login/login.vue'
 import home from '@/components/home/home'
+import UserList from '@/components/user-list/user-list'
 
 Vue.use(Router)
 
@@ -13,7 +14,13 @@ const router = new Router({
     },
     {
       path: '/',
-      component: home
+      component: home,
+      children: [
+        {
+          path: '/users',
+          component: UserList
+        }
+      ]
     }
   ]
 })
